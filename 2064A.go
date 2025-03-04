7afa1a120ab76e9c5ca1d4f67e5fb303690e4847
seikhchilli <saurabhkrsingh2001@gmail.com>
2025-03-04 21:09:00 +0530

    Brogramming Contest

---
+++ 2064A.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+const (
+	MAXINT = int(^uint(0) >> 1)
+)
+
+var in, out = bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout)
+
+func solve() {
+	n := readInt()
+	s := readStr()
+	s = "0" + s
+	ans := 0
+	for i := n - 1; i >= 0; i-- {
+		if s[i] == '0' && s[i+1] == '1' {
+			ans++
+		} else if s[i] == '1' && s[i+1] == '0' {
+			ans++
+		}
+	}
+	print(ans)
+}
+
+func main() {
+	defer out.Flush()
+	var t int
+	for fmt.Fscanln(in, &t); t > 0; t-- {
+		solve()
+	}
+}
+
+type Number interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64 |
+		float32 | float64
+}
+
+type Integer interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64
+}
+
+func readInt() int {
+	var x int
+	fmt.Fscan(in, &x)
+	return x
+}
+
+func readStr() string {
+	var x string
+	fmt.Fscan(in, &x)
+	return x
+}
+
+func readArr[T Integer](x ...T) (T, []T) {
+	var n T
+	if len(x) == 0 {
+		fmt.Fscan(in, &n)
+	} else {
+		n = x[0]
+	}
+	var v = make([]T, n)
+	for i := T(0); i < n; i++ {
+		fmt.Fscan(in, &v[i])
+	}
+	return n, v
+}
+
+func print(x ...any) {
+	fmt.Fprintln(out, x...)
+}
+
+func printSlice[T Number](s []T) {
+	fmt.Fprintln(out, strings.Trim(fmt.Sprint(s), "[]"))
+}
+
+func max[T Number](x ...T) T {
+	mi := 0
+	for i := 1; i < len(x); i++ {
+		if x[i] > x[mi] {
+			mi = i
+		}
+	}
+	return x[mi]
+}
+
+func min[T Number](x ...T) T {
+	mi := 0
+	for i := 1; i < len(x); i++ {
+		if x[i] < x[mi] {
+			mi = i
+		}
+	}
+	return x[mi]
+}
+
+func GCD[T Integer](x, y T) T {
+	x, y = abs(x), abs(y)
+	for y != 0 {
+		x, y = y, x%y
+	}
+	return x
+}
+
+func LCM[T Integer](x, y T) T {
+	return abs(x*y) / GCD(x, y)
+}
+
+func sum[T Number](x ...T) T {
+	res := T(0)
+	for _, i := range x {
+		res += i
+	}
+	return res
+}
+
+func abs[T Number](x T) T {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func reverseInts(x []int) {
+	n := len(x)
+	for i := 0; i < n/2; i++ {
+		x[i], x[n-1-i] = x[n-1-i], x[i]
+	}
+}
+
+func reverseStr(s string) string {
+	x := []rune(s)
+	n := len(x)
+	for i := 0; i < n/2; i++ {
+		x[i], x[n-1-i] = x[n-1-i], x[i]
+	}
+	return string(x)
+}
+
+func binarySearch[T Number](arr []T, target T) int {
+	index := sort.Search(len(arr), func(i int) bool { return arr[i] >= target })
+	if index < len(arr) && arr[index] == target {
+		return index
+	}
+	return -1
+}
